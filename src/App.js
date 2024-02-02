8dbed5ee9752b2ac9e1a354dcd021ffbab5782a7
zachmshort <zach.short@fantomworks.com>
2024-02-01 23:15:43 -0500

    boilerplate css

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="homePage">
+      <div className="myName-Info">
+      <h1 className="myName">Hi! I'm Zachary Short</h1>
+      <p>I have a passion for UI/UX design, 3D modeling/animation, and video editing</p>
+      </div>
+      <div className="ulContainerHomePage">
+        <ul className="ulHomePage">
+          <li>Websites I've Built</li>
+          <li>3D Models I've Built</li>
+          <li>Videos I've Edited</li>
+          <li>Contact Me</li>
+          <li>About Me</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
